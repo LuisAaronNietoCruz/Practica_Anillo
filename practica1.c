800c1fd43c381765d2d52a516715ff79954bd3ec
LuisAaronNietoCruz <100148692+LuisAaronNietoCruz@users.noreply.github.com>
2023-06-02 17:21:59 -0600

    Update practica1.c

--- practica1.c
+++ practica1.c
@@ -5,9 +5,11 @@
 int main(int argc, char** argv) {
     int rank;
     int size;
-    int x0 = 5;
+    int x0 = 1;
     int sum;
-    int m = 10;
+    int m = 2;
+    int total_sum = 0;
+
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -17,11 +19,17 @@ int main(int argc, char** argv) {
 
     for(int i=0; i<m; i++) {
         MPI_Send(&x, 1, MPI_INT, right, 0, MPI_COMM_WORLD);
-        MPI_Recv(&sum, 1, MPI_INT, left, 0, MPI_COMM_WORLD,
-                 MPI_STATUS_IGNORE);
+        MPI_Recv(&sum, 1, MPI_INT, left, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         x += sum;
     }
-    printf("Rank %d: final value = %d\n", rank, x);
+    printf("Rango %d: valor final = %d\n", rank, x);
+
+    MPI_Reduce(&x, &total_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if(rank == 0) {
+        printf("Suma total de los valores finales: %d\n", total_sum);
+    }
+
     MPI_Finalize();
     return 0;
 }
