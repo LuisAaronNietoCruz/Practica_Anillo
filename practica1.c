da786485c2bf6c8ebcfdddbb67c746fdf63d3b0a
LuisAaronNietoCruz <100148692+LuisAaronNietoCruz@users.noreply.github.com>
2023-06-08 22:50:33 -0600

    Update practica1.c

--- practica1.c
+++ practica1.c
@@ -1,35 +1,45 @@
-#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
 
-int main(int argc, char** argv) {
-    int rank;
-    int size;
-    int x0 = 1;
-    int sum;
-    int m = 2;
-    int total_sum = 0;
+int main(int argc, char **argv) {
+  int numproc;
+  int miproc;
+  int x0 = 0;
+  int m = 2;
+  MPI_Status status;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int left = (rank - 1 + size) % size;
-    int right = (rank + 1) % size;
-    int x = x0;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &miproc);
+  MPI_Comm_size(MPI_COMM_WORLD, &numproc);
 
-    for(int i=0; i<m; i++) {
-        MPI_Send(&x, 1, MPI_INT, right, 0, MPI_COMM_WORLD);
-        MPI_Recv(&sum, 1, MPI_INT, left, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        x += sum;
+  if (miproc == 0) {
+    MPI_Send(&x0, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+  }
+
+  for (int cycle = 0; cycle < m; cycle++) {
+    int prev = miproc - 1;
+    if (miproc == 0) {
+      prev = numproc - 1;
     }
-    printf("Rango %d: valor final = %d\n", rank, x);
+    MPI_Recv(&x0, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, &status);
 
-    MPI_Reduce(&x, &total_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-
-    if(rank == 0) {
-        printf("Suma total de los valores finales: %d\n", total_sum);
+    int i = prev + 1 + numproc * cycle;
+    x0 += i;
+    int next = miproc + 1;
+    if (miproc == numproc - 1) {
+      next = 0;
     }
 
-    MPI_Finalize();
-    return 0;
+    MPI_Send(&x0, 1, MPI_INT, next, 0, MPI_COMM_WORLD);
+  }
+
+  if (miproc == 0) {
+    printf("Suma total de los valores finales: %d\n", x0);
+  }
+
+//printf("Rango: %d/n", "Numero de procesos: %d/n", miproc, numproc);
+
+  MPI_Finalize();
+  return 0;
 }
